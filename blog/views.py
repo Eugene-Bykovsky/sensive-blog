c7a43d3f6774916060ccada05320105205db790f
Evgenii Bykovskii <bykovsky.ssau@gmail.com>
2025-02-26 09:16:40 +0400

    add template def serialize_post_optimized

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,22 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments.count(),
+        # Убираем отдельный SQL-запрос
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.first().title if post.tags.exists() else None,
+        # Безопасный доступ к первому тегу
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -41,9 +57,10 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post)
+                       for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
